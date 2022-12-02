bd9ca6573c6caf24a47cf7aa83d072cf795e38ea
limalsha umod <limalshau@gmail.com>
2022-12-02 18:15:28 +0530

    Updated the time and date

--- src/com/onpremiss/pos/controller/DashbordFormController.java
+++ src/com/onpremiss/pos/controller/DashbordFormController.java
@@ -1,8 +1,38 @@
 package com.onpremiss.pos.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DashbordFormController {
+    public AnchorPane dashbordContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+
+    private void setDateAndTime() {
+        //set time
+        Timeline time = new Timeline(
+                new KeyFrame(Duration.ZERO, e->{
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    lblDate.setText(LocalDateTime.now().format(formatter));
+                }), new KeyFrame(Duration.seconds(1)));
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+
+    }
+
+
     public void openCustomerFormOnAction(ActionEvent actionEvent) {
     }
 
