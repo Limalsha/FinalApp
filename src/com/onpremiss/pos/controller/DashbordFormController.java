e4f4f840c1617c9475f204fee8f1fa6ccd32545c
limalsha umod <limalshau@gmail.com>
2022-12-02 13:41:20 +0530

    Updated the code

---
+++ src/com/onpremiss/pos/controller/DashbordFormController.java
@@ -0,0 +1,20 @@
+package com.onpremiss.pos.controller;
+
+import javafx.event.ActionEvent;
+
+public class DashbordFormController {
+    public void openCustomerFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openItemFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openPlaceOrderFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openOrderDetailsFormOnAction(ActionEvent actionEvent) {
+    }
+    private void setUi(String ui){
+
+    }
+}
---
+++ src/com/onpremiss/pos/controller/ItrmFormContoller.java
@@ -0,0 +1,4 @@
+package com.onpremiss.pos.controller;
+
+public class ItrmFormContoller {
+}
