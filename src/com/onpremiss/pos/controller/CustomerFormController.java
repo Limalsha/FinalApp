b71c8bf8ba402670793f306db3d5415d24d03760
limalsha umod <limalshau@gmail.com>
2022-12-03 14:12:12 +0530

    Updated the CustomerFormController

--- src/com/onpremiss/pos/controller/CustomerFormController.java
+++ src/com/onpremiss/pos/controller/CustomerFormController.java
@@ -2,15 +2,28 @@ package com.onpremiss.pos.controller;
 
 import com.onpremiss.pos.db.Database;
 import com.onpremiss.pos.model.Customer;
+import com.onpremiss.pos.view.tm.CustomerTm;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.Observable;
 
 public class CustomerFormController {
     public TextField txtId;
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+
+public void initialize(){
+    searchCustomer();
+}
+ private void searchCustomer(){
+     ObservableList<CustomerTm>
+ }
+
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
         Customer c1 = new Customer(txtId.getText(),txtName.getText(),txtAddress.getText(), Double.parseDouble(txtSalary.getText()));
