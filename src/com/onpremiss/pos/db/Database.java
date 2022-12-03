8c43f72f86504bd0a8514008874a45b72aa1fc85
limalsha umod <limalshau@gmail.com>
2022-12-03 10:15:49 +0530

    Updated the Customer class

--- src/com/onpremiss/pos/db/Database.java
+++ src/com/onpremiss/pos/db/Database.java
@@ -1,4 +1,11 @@
 package com.onpremiss.pos.db;
 
+import com.onpremiss.pos.model.Customer;
+
+import java.util.ArrayList;
+
 public class Database {
+    public static ArrayList<Customer> customerTable
+            = new ArrayList<Customer>();
+
 }
